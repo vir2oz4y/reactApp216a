e6ded28fbebea2392b9e5f1b1b77a04ad1807f61
blinpolina <polina.snegiryova@gmail.com>
2023-04-05 11:38:55 +0700

    some changes

--- src/Pages/Route/RouteComponent.tsx
+++ src/Pages/Route/RouteComponent.tsx
@@ -15,6 +15,13 @@ import BrikkerSashaPage from "../StudentPage/BrikkerSasha/BrikkerSashaPage";
 import ZhukovaLizaPage from "../StudentPage/ZhukovaLiza/ZhukovaLizaPage";
 import BaidinaElenaPage from "../StudentPage/BaidinaElena/BaidinaElenaPage";
 import SysolovNikitaPage from "../StudentPage/SysolovNikita/SysolovNikitaPage";
+import Snegirevatest2Page from "../StudentPage/SnegirevaPolina/test2/Snegirevatest2Page";
+import Snegirevatest1Page from "../StudentPage/SnegirevaPolina/test1/Snegirevatest1Page";
+import Snegirevapostavschiki from "../StudentPage/SnegirevaPolina/postavschiki/Snegirevapostavschiki";
+import Snegirevatovari from "../StudentPage/SnegirevaPolina/tovari/Snegirevatovari";
+import Snegirevaclients from "../StudentPage/SnegirevaPolina/clients/Snegirevaclients";
+import Snegirevazakazi from "../StudentPage/SnegirevaPolina/zakazi/Snegirevazakazi";
+import Snegirevakategorii from "../StudentPage/SnegirevaPolina/kategorii/Snegirevakategorii";
 
 
 const RouteComponent = () => {
@@ -23,17 +30,27 @@ const RouteComponent = () => {
         <HashRouter>
             <Routes>
                 <Route path="/" element={<MainPage/>}>
-                    <Route index element={<StudentsPage/>} />
+                    <Route index element={<StudentsPage/>}/>
 
-                    <Route path={'teacher'} element={<KryuchkovNickPage />} />
-                    <Route path={'akyuzleyla'} element={<AkyuzLeylaPage />} />
-                    <Route path={'brikkersasha'} element={<BrikkerSashaPage />} />
-                    <Route path={'SnegirevaPolina'} element={<SnegirevaPolinaPage/>} />
-                    <Route path={'MuzychenkoStacy'} element={<MuzychenkoStacyPage/>} />
-                    <Route path={'ZhukovaLiza'} element={<ZhukovaLizaPage/>} />
-                    <Route path={'Baidina Elena'} element={<BaidinaElenaPage/>} />
-                    <Route path={'SysolovNikita'} element={<SysolovNikitaPage/>} />
-                    <Route path={'vik'} element={<TitovecVikPage />} />
+                    <Route path={'teacher'} element={<KryuchkovNickPage/>}/>
+                    <Route path={'akyuzleyla'} element={<AkyuzLeylaPage/>}/>
+                    <Route path={'brikkersasha'} element={<BrikkerSashaPage/>}/>
+                    <Route path={'SnegirevaPolina'} element={<SnegirevaPolinaPage/>}>
+                        <Route path={'test1'} element={<Snegirevatest1Page/>}/>
+                        <Route path={'test2'} element={<Snegirevatest2Page/>}/>
+                        <Route path={'postavschiki'} element={<Snegirevapostavschiki/>}/>
+                        <Route path={'tovari'} element={<Snegirevatovari/>}/>
+                        <Route path={'clients'} element={<Snegirevaclients/>}/>
+                        <Route path={'zakazi'} element={<Snegirevazakazi/>}/>
+                        <Route path={'kategorii'} element={<Snegirevakategorii/>}/>
+
+                    </Route>
+
+                    <Route path={'MuzychenkoStacy'} element={<MuzychenkoStacyPage/>}/>
+                    <Route path={'ZhukovaLiza'} element={<ZhukovaLizaPage/>}/>
+                    <Route path={'Baidina Elena'} element={<BaidinaElenaPage/>}/>
+                    <Route path={'SysolovNikita'} element={<SysolovNikitaPage/>}/>
+                    <Route path={'vik'} element={<TitovecVikPage/>}/>
 
                 </Route>
             </Routes>
---
+++ src/Pages/StudentPage/SnegirevaPolina/test2/Snegirevatest2Page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Snegirevatest2Page = () => {
+    return (
+        <div>
+            test2
+        </div>
+    );
+};
+
+export default Snegirevatest2Page;
