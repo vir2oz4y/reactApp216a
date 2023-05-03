307406fc5c674415b570f0b2eae47e8ce8167f0b
{Policken66} <komlev.artem.02@mail.ru>
2023-05-03 10:42:03 +0700

    dd

--- src/Pages/StudentPage/KomlevArtem/KomlevArtemPage.tsx
+++ src/Pages/StudentPage/KomlevArtem/KomlevArtemPage.tsx
@@ -3,7 +3,9 @@ import Header from "../../../Components/Header/Header";
 import ContentBlock from "../../../Components/ContentBlock/ContentBlock";
 import {List, ListItem, ListItemButton, ListItemText} from "@mui/material";
 import {Outlet, useNavigate} from "react-router-dom";
+import axios from "axios";
 
+const komlevAxios = axios.create();
 
 const KomlevArtemPage = () => {
 
