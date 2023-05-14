8828544fab1d1d177937f1ffe0d77757dbae6c57
MarkAlex22 <124755499+MarkAlex22@users.noreply.github.com>
2023-05-14 23:43:34 +0700

    Add files via upload

---
+++ CreateCategoryPopup.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import KryuchkovPopup, { IPopup } from "../../../../../Components/Kryuchkov/KryuchkovPopup/KryuchkovPopup";
+import { Button, TextField } from "@mui/material";
+import { Category } from "../models";
+
+type Props = IPopup & {
+    onCreate:(category:Category) =>void;
+}
+const CreateCategoryPopup = ({open, onClose, onCreate}:Props) => {
+
+    const [categoryName, setCategoryName] = useState('')
+
+    const onCreateClick = () =>{
+        onCreate({
+            id:Math.random(),
+            name:categoryName
+        })
+        onClose();
+    }
+
+    return (
+        <MarkAlex22Popup
+            open={open}
+            onClose={()=>onClose()}
+            title={'Создание категории'}
+        >
+            <div style={{display:'flex', flexDirection:'column', gap:'1em'}}>
+                <TextField
+                    label="Название категории"
+                    variant="standard"
+                    value={categoryName}
+                    onChange={(e)=>
+                        setCategoryName(e.target.value)
+                    }
+                />
+
+                <div style={{display:'flex', justifyContent: 'center'}}>
+                    <Button
+                        color={'primary'}
+                        variant={'contained'}
+                        onClick={()=>onCreateClick()}
+                    >
+                        Создать
+                    </Button>
+                </div>
+            </div>
+        </MarkAlex22Popup>
+    );
+};
+
+export default CreateCategoryPopup;
